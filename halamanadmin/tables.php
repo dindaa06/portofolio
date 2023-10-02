6f40e629bd64cb005c7b6c4ad3bf9964c491bbd9
aedills <zaedilfebriansyah@gmail.com>
2023-10-02 23:17:24 +0800

    Add Modal

--- halamanadmin/tables.php
+++ halamanadmin/tables.php
@@ -425,6 +425,27 @@
 
                                 <script src="js/sb-admin-2.min.js"></script>
 
+                                <!-- Modal -->
+                                <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h4 class="modal-title" id="myModalLabel">Ini Judul</h4>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                Disini taro inputan datanya nanti yaa
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
 </body>
 
 </html>
